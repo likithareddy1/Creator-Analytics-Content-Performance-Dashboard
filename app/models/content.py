aa2011a92cef27d0e29b6a539f3b208c99eb0847
likithareddy1 <likithakalluru123@gmail.com>
2026-08-15 15:57:54 +0530

    Add files via upload

---
+++ app/models/content.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Date
+
+from app.db.database import Base
+
+
+class Content(Base):
+    __tablename__ = "content"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    creator_id = Column(Integer, nullable=False, index=True)
+    platform = Column(String(50), nullable=False)
+    content_title = Column(String(255), nullable=False)
+
+    views = Column(Integer, default=0, nullable=False)
+    likes = Column(Integer, default=0, nullable=False)
+    comments = Column(Integer, default=0, nullable=False)
+    shares = Column(Integer, default=0, nullable=False)
+    saves = Column(Integer, default=0, nullable=False)
+    watch_time = Column(Integer, default=0, nullable=False)
+    reach = Column(Integer, default=0, nullable=False)
+
+    published_date = Column(Date, nullable=False)
